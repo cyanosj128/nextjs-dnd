f41842e33b0fb566194bc4e3e300b560e040df1b
Noh Seung Joo <cyanosj128@gmail.com>
2025-07-03 18:21:39 +0900

    feat: implement basic layout structure with Board and Panel components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Board from "@/components/Board";
+import Panel from "@/components/Panel";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <div className="flex flex-col">
+      <div className="p-[16px]">Control Area</div>
+      <div className="flex gap-[8px]">
+        <div className="flex-1 bg-gray-100 p-[16px] h-[100vh]">
+          <Panel />
+        </div>
+        <div className="flex-5 bg-white p-[16px] h-[100vh]">
+          <Board />
+        </div>
+      </div>
+    </div>
   );
 }
